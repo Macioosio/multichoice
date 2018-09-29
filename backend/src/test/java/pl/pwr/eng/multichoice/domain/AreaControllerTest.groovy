09384172ba4305b7c7c213c41bafbf27c668f127
Macioosio <strusiek148@gmail.com>
2018-09-29 13:56:23 +0200

    Added Area tests.

---
+++ backend/src/test/java/pl/pwr/eng/multichoice/domain/AreaControllerTest.groovy
@@ -0,0 +1,91 @@
+package pl.pwr.eng.multichoice.domain
+
+import groovy.json.JsonSlurper
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import pl.pwr.eng.multichoice.domain.area.Area
+import pl.pwr.eng.multichoice.domain.area.AreaController
+import pl.pwr.eng.multichoice.domain.area.AreaService
+import spock.lang.Specification
+
+import static groovy.json.JsonOutput.toJson
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class AreaControllerTest extends Specification {
+
+    private AreaController areaController
+    private AreaService areaService
+    private Area area
+    MockMvc mockMvc
+
+    void setup() {
+        areaController = new AreaController()
+        areaService = Mock(AreaService.class)
+        areaController.areaService = areaService
+        mockMvc = MockMvcBuilders.standaloneSetup(areaController).build()
+
+        area = new Area()
+        area.id = UUID.randomUUID()
+        area.name = "Test Area"
+        areaService.save(area)
+    }
+
+    def "get all areas"() {
+        when:
+            def response = mockMvc.perform(get("/api/areas"))
+        then:
+            1 * areaService.findAll()
+            response.andExpect(status().isOk())
+    }
+
+    def "get by id"() {
+        when:
+            def response = mockMvc.perform(get("/api/areas/" + area.id)).andReturn().response
+            def content = new JsonSlurper().parseText(response.contentAsString)
+        then:
+            1 * areaService.findById(area.id) >> area
+            response.status == OK.value()
+            content.name == ("Test Area")
+
+    }
+
+    def "delete"() {
+        when:
+            def response = mockMvc.perform(delete("/api/areas/" + area.id))
+        then:
+            1 * areaService.findById(area.id) >> area
+            response.andExpect(status().isOk())
+    }
+
+    def "post"(){
+        given:
+            Map request = [
+                    name : "New Area"
+            ]
+        when:
+            def response = mockMvc.perform(post("/api/areas")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(toJson(request)))
+                    .andReturn().response
+        then:
+            response.status == OK.value()
+    }
+    def "put"(){
+        given:
+        Map request = [
+                id : area.id,
+                name : "Modified Area"
+        ]
+        when:
+        def response = mockMvc.perform(put("/api/areas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(request)))
+                .andReturn().response
+        then:
+        response.status == OK.value()
+    }
+
+}
---
+++ backend/src/test/java/pl/pwr/eng/multichoice/domain/AreaServiceTest.groovy
@@ -0,0 +1,66 @@
+package pl.pwr.eng.multichoice.domain
+
+import pl.pwr.eng.multichoice.domain.area.Area
+import pl.pwr.eng.multichoice.domain.area.AreaRepository
+import pl.pwr.eng.multichoice.domain.area.AreaService
+import spock.lang.Specification
+
+class AreaServiceTest extends Specification {
+
+    private AreaService areaService;
+    private AreaRepository areaRepository;
+    private Area area;
+
+    void setup() {
+        areaService = new AreaService()
+        areaRepository = Mock(AreaRepository.class)
+        areaService.areaRepository = areaRepository
+
+        area = new Area()
+        area.id = UUID.randomUUID()
+        area.name = "Test Area"
+        areaService.save(area)
+    }
+
+    def "Find All"() {
+        when:
+            areaService.findAll()
+        then:
+            1 * areaRepository.findAll()
+    }
+
+    def "Find by Id"() {
+        when:
+            areaService.findById(area.id)
+        then:
+            1 * areaRepository.findById(area.id)
+    }
+
+    def "Save"() {
+        when:
+            areaService.save(area)
+        then:
+            1 * areaRepository.save(area)
+    }
+
+    def "Modify cinema"(){
+        given:
+            Area modifiedArea = new Area(area.id, area.name)
+            modifiedArea.name = "Modified Area"
+        when:
+            areaRepository.findById(modifiedArea.id) >> area
+            areaService.modifyArea(modifiedArea)
+        then:
+            area.name.is(modifiedArea.name)
+            1 * areaRepository.save(area)
+
+    }
+
+    def "Delete"() {
+        when:
+            areaService.delete(area)
+        then:
+            1 * areaRepository.delete(area)
+    }
+
+}
