5fcd9b1a7c1c53796be506bc99026e4a67a8827a
Macioosio <strusiek148@gmail.com>
2018-09-30 22:57:55 +0200

    Basic communication with API.

--- frontend/src/components/Questions.vue
+++ frontend/src/components/Questions.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
     <p>Pytania</p>
+    <button class=”Search__button” @click="callRestService()">CALL Spring Boot REST backend service</button>
+
+    <h3>{{ response }}</h3>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Questions'
+  name: 'Questions',
+  methods: {
+    callRestService
+  },
+
+  data () {
+    return {
+      response: [],
+      errors: []
+    }
+  }
+}
+function callRestService () {
+  axios.get(`api/courses`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.response = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
 }
 </script>
 
