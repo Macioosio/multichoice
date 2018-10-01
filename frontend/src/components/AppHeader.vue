c4b4a9db68d19216085ff0a884bfa830aed3c2d2
Macioosio <strusiek148@gmail.com>
2018-10-01 21:15:14 +0200

    Changed header.

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,24 +1,12 @@
 <template>
 <div>
-  <h1 v-html="polonizeRouteName($route.name)"></h1>
+  <h1>MULTICHOICE</h1>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'AppHeader',
-  methods: {
-    polonizeRouteName
-  }
-}
-function polonizeRouteName (routeName) {
-  switch (routeName) {
-    case 'Questions': return 'Pytania'
-    case 'Tests': return 'Testy'
-    case 'Home': return 'Strona główna'
-    case 'Profile': return 'Profil'
-    default: return 'Unknown'
-  }
+  name: 'AppHeader'
 }
 </script>
 
