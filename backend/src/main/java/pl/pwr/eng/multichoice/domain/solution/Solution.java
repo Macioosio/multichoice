69d1564ef4a45b30faf7b3bf1dfc20693dda48ec
Macioosio <strusiek148@gmail.com>
2018-10-27 20:02:55 +0200

    Added Solution model.

---
+++ backend/src/main/java/pl/pwr/eng/multichoice/domain/solution/Solution.java
@@ -0,0 +1,39 @@
+package pl.pwr.eng.multichoice.domain.solution;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.pwr.eng.multichoice.domain.answer.Answer;
+import pl.pwr.eng.multichoice.domain.student.Student;
+import pl.pwr.eng.multichoice.domain.test.Test;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Solution {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotNull
+    @OneToMany
+    private Student author;
+
+    @NotNull
+    @ManyToOne
+    private Test test;
+
+    @ManyToMany
+    private List<Answer> answers;
+
+
+    private int points;
+}
