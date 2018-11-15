aa82642a706fdbe1f0fbc4e8ffdeaf2eeb3c1596
Macioosio <strusiek148@gmail.com>
2018-11-15 14:32:03 +0100

    Finished results view for teacher.

--- frontend/src/components/TestResults.vue
+++ frontend/src/components/TestResults.vue
@@ -4,33 +4,48 @@
       <div class="md-layout-item md-size-20">
         <div class="small-padding-class">
           <label class="label">Kurs</label>
-          {{course}}
+          <p class="card">
+            {{course.name}}</p>
         </div>
         <div class="small-padding-class">
           <label class="label">Start</label>
+          <div class="card">
           {{start}}
+          </div>
           <label class="label">Koniec</label>
+          <div class="card">
           {{end}}
+          </div>
         </div>
         <div class="small-padding-class">
           <label class="label">Metoda oceniania</label>
-          {{gradingMethod}}
+          <div class="card">{{gradingMethod}}</div>
         </div>
         <label class="label">Nawigowalność</label>
         <div>
-          {{navigable}}
+          <div class="card"> {{navigable}}</div>
         </div>
         <div class="small-padding-class">
           <label class="label">Punkty</label>
-          {{points}}
+          <div class="card">{{points}}</div>
         </div>
         <div class="small-padding-class">
           <router-link class="button" to="/tests">Wróć</router-link>
         </div>
       </div>
-      <div class="md-layout-item">
-      </div>
-      <div class="md-layout-item">
+      <div class="md-layout-item md-size-80">
+        <md-table v-model="solutions" md-card md-fixed-header>
+          <md-table-toolbar>
+            <h1 class="md-title md-toolbar-section-start">Rozwiązania</h1>
+            <h2 md-title md-toolbar-section-end>Średnia: {{meanPoints}} punktów</h2>
+          </md-table-toolbar>
+          <md-table-row slot="md-table-row" slot-scope="{ item }">
+            <md-table-cell md-label="Autor">{{ item.author.email }}</md-table-cell>
+            <md-table-cell md-label="Punkty">{{item.points}}</md-table-cell>
+            <md-table-cell md-label="Procenty">{{calculatePercent(item.points)}}%</md-table-cell>
+            <md-table-cell md-label="Przesłane">{{stringify(item.posted)}}</md-table-cell>
+          </md-table-row>
+        </md-table>
       </div>
     </div>
   </div>
@@ -51,6 +66,7 @@ export default {
       gradingMethod: '',
       navigable: true,
       points: 0,
+      meanPoints: 0,
       questions: [],
       students: [],
       solutions: []
@@ -83,10 +99,18 @@ export default {
     },
     setSolutionsData (data) {
       this.solutions = data
+      let sumPoints = data.map(solution => solution.points).reduce((p, c) => p + c, 0)
+      this.meanPoints = sumPoints / data.length
     },
     convertDate (dateJavaFormat) {
       // 2018-10-19T10:00:00.628+0000
       return moment(dateJavaFormat).format('YYYY-MM-DD HH:mm')
+    },
+    calculatePercent (points) {
+      return (points / this.points) * 100
+    },
+    stringify (boolean) {
+      return boolean ? 'Tak' : 'Nie'
     }
   }
 }
